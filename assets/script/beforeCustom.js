10bb650d944dd156b60f19f6e6cac7c28913ac51
IMRAN MALIK <imran@imranamanat.com>
2017-12-21 15:04:08 +0500

    final touch

--- assets/script/beforeCustom.js
+++ assets/script/beforeCustom.js
@@ -11,7 +11,8 @@
 /* global windows */
 
 
-(function(){ var widget_id = 'Ug4KlqcjlN';var d=document;var w=window;function l(){
+
+(function(){ var widget_id = 'A3ysxQVOEX';var d=document;var w=window;function l(){
 var s = document.createElement('script'); s.type = 'text/javascript'; s.async = true; s.src = '//code.jivosite.com/script/widget/'+widget_id; var ss = document.getElementsByTagName('script')[0]; ss.parentNode.insertBefore(s, ss);}if(d.readyState=='complete'){l();}else{if(w.attachEvent){w.attachEvent('onload',l);}else{w.addEventListener('load',l,false);}}})();
 
 
